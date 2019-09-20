1aed23a39806fbaf42e1a3a5fe914ab7c2129af0
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-09-20 17:10:27 +0900

    user追加テーブル・ボタン、モーダルの追加

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,23 +1,77 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Users Table</h3>
+          <div class="card-tools">
+            <button class="btn btn-success" data-toggle="modal" data-target="#addNewModal">Add New <i class="fas fa-user-plus fa-fw"></i></button>
+          </div>
         </div>
+
+        <!-- /.card-header -->
+        <div class="card-body table-responsive p-0">
+          <table class="table table-hover">
+            <tbody>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Type</th>
+                <th>Registered At</th>
+                <th>Modify</th>
+              </tr>
+              <tr>
+                <td>4290</td>
+                <td>二郎</td>
+                <td>ziro@ss.com</td>
+                <td>たこ</td>
+                <td><span class="tag tag-success">Approved</span>
+                  <a href="#">
+                    <i class="fa fa-edit blue"></i>
+                  </a>
+                  /
+                  <a href="#">
+                    <i class="fa fa-trash red"></i>
+                  </a>
+
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+  </div>
+
+  <!-- Modal -->
+  <div class="modal fade" id="addNewModal" tabindex="-1" role="dialog" aria-labelledby="addNewModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="addNewModalLabel">Add New</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Create</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+export default {
+  mounted() {
+    console.log('Component mounted.')
+  }
+}
 </script>
